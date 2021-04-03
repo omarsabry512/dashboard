fcb958a4730aa03539c2ad8a370360ee2290e9ae
omarsabry512 <omarsabry512@gmail.com>
2021-04-03 02:59:44 +0200

    updates on dashboard

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+    $('.table-head').css('width',`${$('.table-parent').width()}px`);
 
-    $('.form-parnet').css('padding-bottom',`${$('.bottom-form').height()}px`);
+    $('.menu-btn').click(function() {
+        $('.Dashboard .side-menu').toggleClass('show')
+        $('.menu-close').toggleClass('show')
+    })
+
+    $('.menu-close').click(function() {
+        $('.Dashboard .side-menu').removeClass('show')
+        $(this).removeClass('show')
+    })
 });
+
+$(window).resize(function(){
+    $('.table-head').css('width',`${$('.table-parent').width()}px`);
+})
